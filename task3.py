205371c9bd746e2669f423871e9f7b7dcf00b70d
YashovardhanPatil07 <yp5668@rit.edu>
2025-10-07 19:56:41 +0400

    task 3 first commit almost no docstrings

---
+++ task3.py
@@ -0,0 +1,44 @@
+#task 3
+
+def calculate_average_books(filename):
+    """
+    To calculate the average number of books borrowed.
+    
+    Args:
+        filename: Path to the file containing book borrowing data
+    
+    Returns:
+        the average numbers of books borrowed. 
+    """
+    try:
+        filer = open(filename, 'r')
+        
+        total_books = 0
+        valid_count = 0
+        filer.seek(28,0)
+
+        for line in filer:
+            line = line.strip()
+            parts = line.split(',')
+            if len(parts) == 3 and int(parts[1])>=0 and len(parts[2])==10 and parts[2][4]==parts[2][7]=="-":
+                try:
+                    books = int(parts[1].strip())
+
+                    total_books += books
+                    valid_count += 1
+                except (ValueError):
+                    continue
+            
+    except FileNotFoundError:
+        print(f"Error: File {filename} not found.")
+    except Exception as e:
+        print(f"Error reading line: {e}")
+    return total_books/valid_count
+
+
+def main(): #calling the main function
+    print(calculate_average_books("borrowers.csv"))
+
+
+if __name__ == "__main__":
+    main()
